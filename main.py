09cba43451fd58ca98ea649747c3dba63c07bea6
Deepesh456 <deepeshkumark120@gmail.com>
2025-07-07 14:05:39 +0530

    Added greeting print to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from recommender.data_loader import load_outfit_data
 from recommender.model import recommend_outfit
 
+import pandas as pd
+age=pd.array(1,2,3)
+
 if __name__ == "__main__":
     outfit_data = load_outfit_data("data/outfits.csv")
     user_input = {'occasion': 'casual'}
     outfit = recommend_outfit(user_input, outfit_data)
     print("Recommended Outfit:\n", outfit)
-#project
